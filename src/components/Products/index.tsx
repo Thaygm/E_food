a080fdb75405a4f7685993ddda265b98fa79eb55
ThayMendes <thay.gm@live.com>
2024-05-09 14:27:05 -0300

    first commit

---
+++ src/components/Products/index.tsx
@@ -0,0 +1,29 @@
+import { BottomCard, Card, CardHeader, Description, Title } from './styles'
+import Button from '../Button'
+
+type ProductsProps = {
+  title: string
+  description: string
+  image: string
+}
+
+const Products = ({ title, description, image }: ProductsProps) => (
+  <Card>
+    <img src={image} alt={title} />
+    <BottomCard>
+      <CardHeader>
+        <Title>{title}</Title>
+      </CardHeader>
+      <Description>{description}</Description>
+      <Button
+        variant="secundary"
+        to="/product"
+        type={'button'}
+        title={'Adicione ao carrinho'}
+        children={'Adicionar ao carrinho'}
+      />
+    </BottomCard>
+  </Card>
+)
+
+export default Products
